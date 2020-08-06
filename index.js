1ac1e13128004d2e0f1f770e112e6da49525da91
Jocelyn Thurn <loaferspenny@gmail.com>
2020-08-06 17:11:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ if(string.toLowerCase() === string){
 }  else if(string.toUpperCase() === string) {
     return "YES INDEED!";
   } else if(string === "I love you, Grandma."){
-    return "I love you, too";
+    return "I love you, too.";
   }
 }
