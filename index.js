9bf167c6aa561ba0c05279346be04d70c00abee8
Jocelyn Thurn <loaferspenny@gmail.com>
2020-08-06 17:07:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string){
-if(sayHiToGrandma.toLowerCase === lowercase){
+if(sayHiToGrandma.toLowerCase === string){
   return "I can't hear you!";
-}  else if(sayHiToGrandma.toUpperCase === uppercase) {
+}  else if(sayHiToGrandma.toUpperCase === string) {
     return "YES INDEED!";
   } else if(string === "I love you, Grandma."){
     return "I love you, too";
